59ce82bb69b04477d03464b232fbd0f98322c6ca
billchen-818 <chenmingxing001@gmail.com>
2021-01-11 18:31:29 +0800

    修改htlc链码json参数名

--- chaincode/htlc/htlc.go
+++ chaincode/htlc/htlc.go
@@ -21,7 +21,7 @@ type HTLC struct {
 
 type Account struct {
 	Address  string `json:"address"`
-	Amount   uint64 `json:"tokens"`
+	Amount   uint64 `json:"amount"`
 	Passwd   string `json:"passwd"`
 	Sequence uint64 `json:"sequence"`
 }
