2ac4154d2fe6997d1ba51d05144c1ff2df21af8e
ZJJ-XYH <zhoujunjie1031@126.com>
2021-01-28 17:47:49 +0800

    修改geth下载版本号

--- htlc-eth/launch.sh
+++ htlc-eth/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ARCH=$(echo "$(uname -s|tr '[:upper:]' '[:lower:]'|sed 's/mingw64_nt.*/windows/')-$(uname -m | sed 's/x86_64/amd64/g')")
-VERSION=1.9.26-unstable-8d62ee65
+VERSION=1.9.0-52f24617
 BINARY_FILE=geth-${ARCH}-${VERSION}.tar.gz
 URL=https://gethstore.blob.core.windows.net/builds/${BINARY_FILE}
 
