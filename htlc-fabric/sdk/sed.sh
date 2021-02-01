da42ca89325d83563bc1963c3fc640d0209eeb49
billchen-818 <chenmingxing@ehousechina.com>
2021-02-01 02:40:51 +0000

    修改GOPATH目录为PWD目录

--- htlc-fabric/sdk/sed.sh
+++ htlc-fabric/sdk/sed.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ARCH=$(uname -s | grep Darwin)
-NEWSTR=`echo ${GOPATH//\//\\\/}`
-OLDSTR='s/${GOPATH}/'
+NEWSTR=`echo ${PWD//\//\\\/}`
+echo NEWSTR
+OLDSTR='s/${PWD}/'
+echo OLDSTR
 if [ "$ARCH" == "Darwin" ]; then
   sed -i "" $OLDSTR$NEWSTR/g config.yaml
 else
