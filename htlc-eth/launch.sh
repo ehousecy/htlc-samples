a504fdab6000e0217e24bb006e8eba8d5471c3c5
ZJJ-XYH <zhoujunjie1031@126.com>
2021-02-01 18:33:09 +0800

    添加gitignore,以太运行脚本添加geth执行权限

--- htlc-eth/launch.sh
+++ htlc-eth/launch.sh
@@ -30,6 +30,7 @@ initDatadir() {
 startNode() {
     enterDatadir
     echo "--------------------Start ETH Node--------------------"
+    chmod +x geth
     nohup ./geth --rpc --rpcport "8545" --rpccorsdomain "*" --datadir "./data0" --port "30303"  --networkid 100000 \
     --allow-insecure-unlock --etherbase 0x93ee701C44f9aa98086685c3AC5810f79762202d  \
     --mine --miner.threads=8 console 2>&1 &
