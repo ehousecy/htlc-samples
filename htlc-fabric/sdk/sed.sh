d91625a1e5c9ab628dd53595494725402a4fdddc
ZJJ-XYH <zhoujunjie1031@126.com>
2021-02-01 18:35:57 +0800

    添加git忽略文件,去除多余打印信息

--- htlc-fabric/sdk/sed.sh
+++ htlc-fabric/sdk/sed.sh
@@ -2,9 +2,7 @@
 
 ARCH=$(uname -s | grep Darwin)
 NEWSTR=`echo ${PWD//\//\\\/}`
-echo NEWSTR
 OLDSTR='s/${PWD}/'
-echo OLDSTR
 if [ "$ARCH" == "Darwin" ]; then
   sed -i "" $OLDSTR$NEWSTR/g config.yaml
 else
